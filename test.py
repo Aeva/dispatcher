03c9d851388533ad37781175c55a109f7aa96f8a
Aeva Palecek <aeva.ntsc@gmail.com>
2013-11-24 18:37:58 -0600

    added license notice etc

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# This file is part of Dispatcher
+#
+# Dispatcher is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Dispatcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Dispatcher.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys, os.path, json, time, random
 import gi
 gi.require_version("WebKit", "3.0")
